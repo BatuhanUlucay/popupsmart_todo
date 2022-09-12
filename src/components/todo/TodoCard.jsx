ee1462325e404e1e57e6a0afa45d2639d7d0e809
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-09-12 09:08:29 +0300

    icons for update and delete buttons are added.

--- src/components/todo/TodoCard.jsx
+++ src/components/todo/TodoCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { HiPencilAlt, HiOutlineTrash } from "react-icons/hi";
 
 function TodoCard({ todo }) {
   return (
     <>
       <div className="todo__card">
         <input className="checkbox" type={"checkbox"} />
-        <div className="content-container">
-          <div className="todo-content">{todo.content}</div>
+        <div className="todo-content">{todo.content}</div>
+        <div className="update__delete">
+          <HiPencilAlt size={"30px"} style={{marginBottom: "1.5rem"}}/>
+          <HiOutlineTrash size={"30px"}/>
         </div>
       </div>
     </>
