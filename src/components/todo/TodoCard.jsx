51b22192de5c7d62be4ba86d19e06d17c84c745b
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-09-13 16:11:08 +0300

    update is implemented.

--- src/components/todo/TodoCard.jsx
+++ src/components/todo/TodoCard.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiPencilAlt, HiOutlineTrash } from "react-icons/hi";
 import Todos from "./Todos";
 
 function TodoCard({ todo, todos, setTodos }) {
+  const [contentMode, setContentMode] = useState("");
+  const [inputMode, setInputMode] = useState("hidden");
+  const [updatedContent, setUpdatedContent] = useState("");
+
   const handleDelete = (e) => {
     fetch(`https://631ae431dc236c0b1ee6b313.mockapi.io/todos/${todo.id}`, {
       method: "DELETE",
@@ -12,13 +16,47 @@ function TodoCard({ todo, todos, setTodos }) {
     });
   };
 
-  const handleUpdate = (e) => {};
+  const handleUpdate = (e) => {
+    setInputMode("");
+    setContentMode("hidden");
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault();
+    const index = todos.findIndex((element) => element.id === todo.id);
+    todos[index].content = updatedContent;
+
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(todos[index]),
+    };
+
+    fetch(
+      `https://631ae431dc236c0b1ee6b313.mockapi.io/todos/${todo.id}`,
+      requestOptions
+    ).then(() => {
+      setTodos(todos);
+      setInputMode("hidden");
+      setContentMode("");
+      console.log("Updated!");
+    });
+  };
 
   return (
     <>
       <div className="todo__card">
         <input className="checkbox" type={"checkbox"} />
-        <div className="todo-content">{todo.content}</div>
+        <div className={`todo-content ${contentMode}`}>{todo.content}</div>
+        <div className={`todo-input ${inputMode}`}>
+          <form onSubmit={handleSave}>
+            <input
+              defaultValue={todo.content}
+              onChange={(event) => setUpdatedContent(event.target.value)}
+            ></input>
+            <button>Save</button>
+          </form>
+        </div>
         <div className="update__delete">
           <i className="update-button" onClick={handleUpdate}>
             <HiPencilAlt size={"30px"} style={{ marginBottom: "1.5rem" }} />
