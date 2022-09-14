5cda5c0a8968401b58567f8aaecd8948c92b18c1
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-09-14 11:13:33 +0300

    header is styled.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/images/logo.png";
+import github from "../../assets/images/githubLogo.png";
 
 function Header() {
   return (
-    <div className='header'>
-        {/* <div className='gh-container'>
-            <img src='src/assets/images/GitHub-Mark.png' alt='github'/>
-        </div> */}
+    <div className="header">
+      <img src={logo} alt="logo" className="logo" />
+      <div className="welcome-header">
+        <h1>To Do</h1>
+      </div>
+      <div className="gh-container">
+        <a
+          href="https://github.com/BatuhanUlucay/popupsmart_todo"
+          target="blank_"
+          className="gh-logo"
+        >
+          <img src={github} alt="github" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
